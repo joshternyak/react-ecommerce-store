3487ad8dcbb7978249d653322aefe9fd4ec0ad6a
Josh Ternyak <joshternyak@gmail.com>
2021-04-15 18:28:03 -0500

    added redux and implemented userReducer and userActions to header and app components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { SHOP_DATA } from "../../shop-data"
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+import CollectionPreview from "../../components/collection-preview/collection-preview"
 
 export default class ShopPage extends Component {
   constructor(props) {
